c9536bb0a58453b713e9f54fdfc7a3435ad93d6d
Harrun01 <harunhanduleh04@gmail.com>
2024-11-06 12:55:58 +0000

    EDITS

---
+++ js/exchange.js
@@ -0,0 +1,35 @@
+(function(){
+	// logic here
+ let pricePounds;
+let exchangeRate;
+let priceEuros;
+const convertForm = document.getElementById('myForm');
+const msg = document.getElementById('msg');
+convertForm.addEventListener('submit', (ev) =>{
+	ev.preventDefault();
+    priceEuros = 0;
+    exchangeRate = 0.87;
+    pricePounds = parseFloat(document.getElementById("pounds").value);
+    if(isNaN(pricePounds)){
+        msg.style.display = "block";
+        msg.innerHTML = "You must enter a number";
+        msg.setAttribute('class', 'error');
+    } else {
+
+        if(pricePounds === 0){
+            msg.style.display = "block";
+            msg.innerHTML = "You must enter more than zero."
+            msg.setAttribute('class', 'error');
+        }else{
+            priceEuros = pricePounds * exchangeRate;
+            msg.style.display = "block";
+            priceEuros = priceEuros.toFixed(2);
+            msg.innerHTML = "You will get &euro;"+priceEuros;
+            msg.removeAttribute('class');
+        }
+
+    }
+});
+
+    //end here
+})();
