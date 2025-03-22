c079e1934eede18afd0b21082749908f0514c5e7
Harrun01 <harrunhanduleh04@gmail.com>
2025-03-22 14:39:53 +0000

    practice edits

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ console.log(firstButton); // Logs the first element with the class 'btn'
 let colourButtons = document.querySelectorAll(".colPicker");
 console.dir(colourButtons); // Logs a NodeList of all elements with the class 'colPicker'
 document.getElementById('myTestBtn').addEventListener('click', (ev) => console.dir(ev));
+
+document.querySelector(".red").addEventListener('click', function(){
+         document.body.setAttribute('class', "redBack");
+   })
+   document.querySelector(".blue").addEventListener('click',function(){
+            document.body.setAttribute('class', "blueBack");
+   })
+   document.querySelector(".green").addEventListener('click',function(){
+    document.body.setAttribute('class',"greenBack");
+   })
+    document.querySelector(".reset").addEventListener('click',function(){
+     document.body.setAttribute('class', "resetBack");
+    })
